1fdc09adfd18774ca27ad540c8b0a1cbc82fd94d
Mike Kotte <kotte.mike@gmail.com>
2020-02-10 12:36:55 +0100

    Configured Authorization. Implemented permissions check TG-32 #ready-for-test

---
+++ src/main/java/com/hotspotted/server/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.hotspotted.server.config;
+
+import com.hotspotted.server.config.security.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${OATH2_IDENTIFIER}")
+    private String audience;
+
+    @Value("${OATH2_ISSUER}")
+    private String issuer;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new PermissionsGrantedAuthoritiesConverter());
+
+        http.authorizeRequests()
+                .mvcMatchers("/hotspot/search").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer().jwt().jwtAuthenticationConverter(converter);
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder)
+                JwtDecoders.fromOidcIssuerLocation(issuer);
+
+        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+
+        jwtDecoder.setJwtValidator(withAudience);
+
+        return jwtDecoder;
+    }
+}
