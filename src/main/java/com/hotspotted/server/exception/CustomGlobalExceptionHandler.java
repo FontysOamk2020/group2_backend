183f36ec0432ea09afe0eb3e430f63b4f625f2c4
Mike Kotte <kotte.mike@gmail.com>
2020-02-17 16:23:16 +0100

    Revert "Revert "🌍 Now able to search for HotSpots.""

--- src/main/java/com/hotspotted/server/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/com/hotspotted/server/exception/CustomGlobalExceptionHandler.java
@@ -7,10 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.net.BindException;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
--- src/main/java/com/hotspotted/server/repository/HotSpotRepository.java
+++ src/main/java/com/hotspotted/server/repository/HotSpotRepository.java
@@ -2,10 +2,15 @@ package com.hotspotted.server.repository;
 
 import com.hotspotted.server.entity.HotSpot;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface HotSpotRepository extends JpaRepository<HotSpot, UUID> {
+    @Query(value = "SELECT * FROM hotspot AS H INNER JOIN location AS L on H.location_id = L.id WHERE earth_distance (ll_to_earth(L.latitude, L.longitude), ll_to_earth(:long, :lat)) <= :range", nativeQuery = true)
+    List<HotSpot> findBySearchParams(@Param("long") double longitude, @Param("lat") double latitude, @Param("range") int range);
 }
