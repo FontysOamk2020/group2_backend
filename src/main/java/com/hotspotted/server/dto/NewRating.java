6eb5555c0d648e876075f9fba7822e55af9d19ac
Kim van der Meulen <kim.meulen7@gmail.com>
2020-04-08 12:41:17 +0200

    Data validation rating

--- src/main/java/com/hotspotted/server/dto/NewRating.java
+++ src/main/java/com/hotspotted/server/dto/NewRating.java
@@ -2,9 +2,15 @@ package com.hotspotted.server.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class NewRating {
 
-    // add validation and step here
+    @Min(value = 0, message = "rating can't be lower than 0")
+    @Max(value = 5, message = "rating can't be higher than 5")
+    @NotNull(message = "Please provide: rating")
     double rating;
 }
