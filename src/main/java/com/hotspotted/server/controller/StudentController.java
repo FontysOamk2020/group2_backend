0bb7fdc7d7cc5c84011ca7a9bf70a99a357ecc4d
Kim van der Meulen <kim.meulen7@gmail.com>
2020-04-15 11:02:47 +0200

    av rating working

--- src/main/java/com/hotspotted/server/controller/StudentController.java
+++ src/main/java/com/hotspotted/server/controller/StudentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
-@Tag(name = "student", description = "All endpoints regarding student")
+@Tag(name = "Student", description = "All endpoints regarding student")
 @RequestMapping("/student")
 public class StudentController {
 
